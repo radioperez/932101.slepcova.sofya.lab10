98724990b30afee271e4e79d5c4e5616416fa19e
Slepcova <brostikslavik@yandex.com>
2023-11-11 16:43:32 +0700

    finished tasks 2/3

--- lab10.js
+++ lab10.js
@@ -1,5 +1,6 @@
 const curtain = document.getElementById("curtain");
 const lamp = document.getElementById("lamp");
+const light = document.getElementById("light");
 const wizard = document.getElementById("wizard");
 const hat = document.getElementById("hat");
 const hat_content = document.getElementById("hat-content");
@@ -14,4 +15,11 @@ lamp.addEventListener("mousedown", function(){
 });
 lamp.addEventListener("mouseup", function(){
 	this.src = "lamp1.png";
+});
+
+let lampState = false;
+lamp.addEventListener("click", function() {
+	if (lampState) light.style.opacity = "0";
+	else light.style.opacity = "0.5";
+	lampState = !lampState;
 });
